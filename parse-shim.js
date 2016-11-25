20fec21bce7d09d6b7fc0aa1eb152e2270991c8a
Jacky Wong <jacky.wong@touchhk.com>
2016-11-25 23:27:17 +0800

    Change global parse existence checking

--- parse-shim.js
+++ parse-shim.js
@@ -1,5 +1,5 @@
 // This is a shim that loads the correct version of parse for a given environment
-if (Parse) {
+if ( typeof Parse !== 'undefined' && Parse ) {
   module.exports = Parse;
 } else if (typeof window === 'undefined') {
   module.exports = require('parse/node');
